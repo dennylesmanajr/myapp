0158d8d2d6dbc717f2ffdfc8ea33ff24da4916cf
dennylesmanatrees <denny.lesmana@treessolutions.com>
2020-06-07 10:44:33 +0700

    create login on progress

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { history } from './helpers';
+import { PrivateRoute } from './components/privateRoute';
+import { LoginPage } from "./components/loginComponent";
+// import logo from "./logo.svg";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="jumbotron">
+        <div className="container">
+            <div className="col-sm-8 col-sm-offset-2">
+                {alert.message &&
+                    <div className={`alert ${alert.type}`}>{alert.message}</div>
+                }
+                <Router history={history}>
+                    <div>
+                        <PrivateRoute exact path="/" component={LoginPage} />
+                        <Route path="/login" component={LoginPage} />
+                    </div>
+                </Router>
+            </div>
+        </div>
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  const { alert } = state;
+  return {
+      alert
+  };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+
+export default connectedApp; 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { store } from './helpers';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  </React.StrictMode>,
+  <Provider store={store}>
+        <App />
+  </Provider>,
   document.getElementById('root')
 );
 
